cd652dd76f58dc3265f61896e852f7cd98310ecc
qiuqili <835671347@qq.com>
2018-07-28 17:35:39 -0400

    Add infinite scroll

--- fetch.js
+++ fetch.js
@@ -1,28 +1,35 @@
-function updateFetch(type) {
+var allIds = [];
+var i = 0;
+var type = 'topstories';
+
+function updateFetch() {
   fetch('https://hacker-news.firebaseio.com/v0/' + type + '.json')
     .then(function(response) {
       return response.json();
     })
     .then(function(ids) {
-      return Promise.all(
-        ids.map(function(id) {
-          return fetch('https://hacker-news.firebaseio.com/v0/item/' + id + '.json').then(function(response) {
-            return response.json();
-          });
-        })
-      );
-    })
-    .then(function(items) {
-      items.map(function(item) {
-        createItem(item, type);
-      });
+      allIds = ids;
+      fetchItems(ids.slice(0, 30));
     });
 }
 
-updateFetch('topstories');
+function fetchItems(ids) {
+  return Promise.all(
+    ids.map(function(id) {
+      return fetch('https://hacker-news.firebaseio.com/v0/item/' + id + '.json').then(function(response) {
+        return response.json();
+      });
+    })
+  ).then(function(items) {
+    items.map(function(item) {
+      createItem(item);
+    });
+  });
+}
 
-function createItem(value, type) {
-  console.log(type);
+updateFetch();
+
+function createItem(value) {
   var list = document.querySelector('.content');
   var listItem = document.createElement('li');
   var link = document.createElement('a');
@@ -30,15 +37,23 @@ function createItem(value, type) {
   link.href = value.url;
   var sub = document.createElement('div');
   if (type == 'jobstories') {
-    sub.innerHTML = timeConvert(value.time) + ' ago ';
+    sub.innerHTML = timeConvert(value.time);
   } else {
-    sub.innerHTML = value.score + ' points by ' + value.by + timeConvert(value.time) + ' ago | hide | ';
+    sub.innerHTML = value.score + ' points by ' + value.by + timeConvert(value.time) + '| hide | past | web | discuss';
   }
-
   listItem.appendChild(link);
   listItem.appendChild(sub);
   list.appendChild(listItem);
 }
+
+window.addEventListener('scroll', function() {
+  var bottom = document.querySelector('footer');
+  var rect = bottom.getBoundingClientRect();
+  if (window.innerHeight > rect.y) {
+    i += 30;
+    fetchItems(allIds.slice(0 + i, 30 + i), type);
+  }
+});
 
 // time
 function timeConvert(t) {
@@ -48,4 +63,3 @@ function timeConvert(t) {
     time = h + ':' + min + ' ';
   return time;
 }
-// console.log(timeConvert(1532707016));
